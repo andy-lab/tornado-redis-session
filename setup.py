011542be56c23e3e4b6d05aafec2734b9ba382af
AndyLab <dooostyle@gmail.com>
2019-05-15 16:00:09 +0800

    0.1.3 -> 0.1.3 support rediscluster

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 
 setup(
-    name='tornado-redis-session',
-    packages=['tornado_redis_session'],
+    name='tornado-rediscluster-session',
+    packages=['tornado_rediscluster_session'],
     version='0.1.4',
     description='Server side session middleware based on redis or rediscluster',
     author='David Wong & Andy Wong',
     author_email='stef-hw@163.com & dooostyle@gmail.com',
-    url='https://github.com/andy-lab/tornado-redis-session',
-    #download_url='https://github.com/hw20686832/tornado-redis-session/archive/0.1.3.tar.gz',
+    url='https://github.com/andy-lab/tornado-rediscluster-session',
+    #download_url='https://github.com/andy-lab/tornado-rediscluster-session/archive/0.1.4.tar.gz',
     keywords=['tornado', 'session', 'redis', 'rediscluster'],
     install_requires=['redis', 'rediscluster', 'tornado'],
     classifiers=[],
